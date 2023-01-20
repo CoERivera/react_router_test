9662d12667bda601889d179422ec413366faa7bd
Christopher Rivera <riveramercado.christopher@gmail.com>
2023-01-19 21:58:48 -0400

    Fixed color route, added checks

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,4 +1,4 @@
-import { Params, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 function Color() {
   const { word, color1, color2 } = useParams();
@@ -7,12 +7,12 @@ function Color() {
     isNaN(word) ?
       <div className="card mt-3">
         <div className="card-body">
-          <h1>The word is: {word}</h1>
+          <h1 style={{color: color1, backgroundColor: color2?color2:''}}>The word is: {word}</h1>
         </div>
       </div> :
       <div className="card mt-3">
         <div className="card-body">
-          <h1>The number is: {word}</h1>
+          <h1 style={{color: color1, backgroundColor: color2?color2:''}}>The number is: {word}</h1>
         </div>
       </div>
   )
